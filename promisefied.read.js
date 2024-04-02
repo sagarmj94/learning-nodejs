239c4d24c173583e191294df9e338b0b58a7b899
Sagar Jadhav <sagarmj1502@gmail.com>
2024-04-02 00:37:15 -0700

    using promise read file asynchronously file

---
+++ promisefied.read.js
@@ -0,0 +1,20 @@
+const { convertCsv } = require("./csv.parse");
+const fs = require("fs");
+const { promisify } = require("util");
+
+const readFile = promisify(fs.readFile);
+
+// readFile("./data/pulitzer-circulation-data.csv", "utf8")
+//   .then((data) => {
+//     console.table(convertCsv(data));
+//   })
+//   .catch((err) => {
+//     console.log("🚀 ~ readFile ~ err:", err);
+//   });
+
+const read = async () => {
+  const data = await readFile("./data/pulitzer-circulation-data.csv", "utf8");
+  console.table(convertCsv(data));
+};
+
+read();
