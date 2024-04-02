cc565a4fb18388cd178a88160d4db37fe46650c7
Sagar Jadhav <sagarmj1502@gmail.com>
2024-04-02 05:34:06 -0700

    practice on path module

--- module.js
+++ module.js
@@ -6,3 +6,16 @@
 const { register, login } = require("./auth");
 register("Sagar");
 login("Sagarmj1502");
+
+const path = require("path");
+
+console.log("older name", path.dirname(__filename));
+console.log("base name", path.basename(__filename));
+
+//extension
+console.log("extname___", path.extname(__filename));
+
+// parse
+console.log("parse:", path.parse(__filename));
+//join
+console.log("Join:", path.join(__dirname, "order", "app.js"));
